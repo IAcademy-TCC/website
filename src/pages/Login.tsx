bbf2a479229c523c458dae8a8cc85bf59166183b
Thiago Gerbi <gerbithiago@gmail.com>
2025-04-29 02:10:14 -0300

    Criando o projeto e iniciando a tela de login

---
+++ src/pages/Login.tsx
@@ -0,0 +1,15 @@
+export default function Login(){
+    return (
+        <>
+            <div className="m-30 content-center">
+                <div>
+                    <h2 className="text-cinza-texto text-xl">Bem-vindo ao</h2>
+                    <h1 className="text-foreground font-extrabold text-3xl">IAcademy</h1>
+                </div>
+                <div className="mt-15">
+                    <button className="w-94 h-13 text-cinza-texto text-base border-1 border-borda rounded-xl"> Google</button>
+                </div>
+            </div>
+        </>
+    )
+}
