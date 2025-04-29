7771485a38abe777142c22d24a65d8c596fdf967
Thiago Gerbi <gerbithiago@gmail.com>
2025-04-29 13:33:51 -0300

    Finalizando o visual da tela de login

---
+++ src/components/TopBar.tsx
@@ -0,0 +1,14 @@
+export default function TopBar(){
+    return (
+      <>
+        <div>
+            <a>Inicio</a>
+            <a>Aprendizado</a>
+            <a>Comunidade</a>
+            <a>Ranking</a>
+            <a>Sobre Nós</a>
+            <a>Contato</a>
+        </div>
+      </>  
+    );
+}
