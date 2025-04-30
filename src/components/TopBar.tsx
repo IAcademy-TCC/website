aa8c97f8a1fb71d384612d78b12407e527c1ade6
Thiago Gerbi <gerbithiago@gmail.com>
2025-04-30 00:55:14 -0300

    Adicionando um pouco de estilo e adicionando o input de busca

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,14 +1,18 @@
 export default function TopBar(){
     return (
       <>
-        <div className="flex flex-row m-30">
-            <div className="">
-                <a href="#">Inicio</a>
-                <a href="#">Aprendizado</a>
-                <a href="#">Comunidade</a>
-                <a href="#">Ranking</a>
-                <a href="#">Sobre Nós</a>
-                <a href="#">Contato</a>
+        <div className="flex flex-row w-auto h-15 align-middle">
+            <div className="m-5 ml-30 flex gap-x-6 text-black font-medium">
+                <a href="#" className="hover:text-roxo-claro">Inicio</a>
+                <a href="#" className="hover:text-roxo-claro">Aprendizado</a>
+                <a href="#" className="hover:text-roxo-claro">Comunidade</a>
+                <a href="#" className="hover:text-roxo-claro">Ranking</a>
+                <a href="#" className="hover:text-roxo-claro">Sobre Nós</a>
+                <a href="#" className="hover:text-roxo-claro">Contato</a>
+            </div>
+            <div className="m-5 ml-150">
+              <input type="text" className="w-80 border-1 border-roxo-claro rounded-2xl h-9 placeholder:text-sm p-2 placeholder:opacity-75 focus:border-roxo-claro" 
+              placeholder="Buscar..."/>
             </div>
         </div>
       </>  
